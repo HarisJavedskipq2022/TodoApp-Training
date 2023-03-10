50e20fe406e2c33edc5656b4764ede107bb92ea0
HarisJavedskipq2022 <haris.javed.skipq@gmail.com>
2023-03-10 20:46:27 +0500

    Refactoring code to different layers

---
+++ src/domain/model/User.ts
@@ -0,0 +1,34 @@
+import { Model, DataTypes } from 'sequelize';
+import { User } from '../entity/user';
+import db from '../../config/database.config';
+
+export class UserModel extends Model<User> {
+  password: string | undefined;
+  id: string | undefined;
+  email: string | undefined;
+}
+   
+    UserModel.init(
+      {
+        id: {
+            type: DataTypes.UUIDV4,
+            primaryKey: true,
+            allowNull: false
+        },
+        
+        
+        email : {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+    
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+      },
+      {
+        sequelize: db,
+        tableName: 'usertable'
+      },
+    );
