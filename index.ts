655e52d796f427edd6a138df4fab2e6cc16de10f
Haris Javed <haris.javed.skipq@gmail.com>
2023-04-05 15:53:55 +0500

    adding config file

--- index.ts
+++ index.ts
@@ -1,20 +1,20 @@
 import 'reflect-metadata'
 import express from 'express'
-import 'dotenv/config'
+import config from './config'
 import router from './src/http/route'
 import { Command } from 'commander'
 import { connectionToDb } from './src/infrastructure/utils/connection'
 import SlackNotify from 'slack-notify'
 
-const slack = SlackNotify(process.env.SLACK_WEBHOOK_URL as string)
+const slack = SlackNotify(config.slackWebHookUrl)
 
-slack.send('Hello!')
-      .then(() => {
-            console.log('done!')
-      })
-      .catch((err) => {
-            console.error(err)
-      })
+// slack.send('Hello!')
+//       .then(() => {
+//             console.log('done!')
+//       })
+//       .catch((err) => {
+//             console.error(err)
+//       })
 
 const app = express()
 const program = new Command()
@@ -25,10 +25,10 @@ app.use(express.json())
 app.use('/api/v1/todo/', router)
 
 const a = program.parse(process.argv)
-const port = Number(a.args[0]) || process.env.PORT
+const port = Number(a.args[0]) || config.port
 
 program.option('-p, --port <number>', 'port number')
 
 app.listen(port, () => {
-      console.log(`app is listening on port ${port}`)
+      console.log(`app is listening on port ${config.port}`)
 })
