461c99c56d4317fe514d307e0f23421b280af85b
Haris Javed <haris.javed.skipq@gmail.com>
2023-04-05 14:52:48 +0500

    refactoring code

--- index.ts
+++ index.ts
@@ -4,10 +4,9 @@ import 'dotenv/config'
 import router from './src/http/route'
 import { Command } from 'commander'
 import { connectionToDb } from './src/infrastructure/utils/connection'
-
 import SlackNotify from 'slack-notify'
-const MY_SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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'
-const slack = SlackNotify(MY_SLACK_WEBHOOK_URL)
+
+const slack = SlackNotify(process.env.SLACK_WEBHOOK_URL as string)
 
 slack.send('Hello!')
       .then(() => {
@@ -17,16 +16,17 @@ slack.send('Hello!')
             console.error(err)
       })
 
-const program = new Command()
-
 const app = express()
+const program = new Command()
 
 connectionToDb()
 
 app.use(express.json())
+app.use('/api/v1/todo/', router)
+
 const a = program.parse(process.argv)
 const port = Number(a.args[0]) || process.env.PORT
-app.use('/api/v1/todo/', router)
+
 program.option('-p, --port <number>', 'port number')
 
 app.listen(port, () => {
