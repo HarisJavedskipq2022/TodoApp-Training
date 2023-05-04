2e7d3a544de0bafdf309c7fc3e6a12f0675f77d1
Haris Javed <haris.javed.skipq@gmail.com>
2023-05-04 16:39:44 +0500

    added logger

--- bin/index.ts
+++ bin/index.ts
@@ -7,6 +7,7 @@ import { connectionToDb } from '../src/infrastructure/database/connection'
 import 'dotenv/config'
 import config from '../src/infrastructure/config'
 import cors from 'cors'
+import logger from '../src/infrastructure/config/logger'
 
 const app = express()
 app.use(cors())
@@ -18,11 +19,27 @@ app.use(express.json())
 app.use('/api/v1', todoRouter)
 app.use('/api/v1', userRouter)
 
+// for logging requests
+app.use((req, res, next) => {
+  const start = Date.now()
+  res.on('finish', () => {
+    const duration = Date.now() - start
+    logger.info(`HTTP ${req.method} ${req.url} ${res.statusCode} ${duration}ms`)
+  })
+  next()
+})
+
+//for logging errors
+app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  logger.error('An error occurred', { message: err.message, stack: err.stack })
+  res.status(500).send('An error occurred')
+})
+
 const a = program.parse(process.argv)
 const port = Number(a.args[0]) || config.ports.port
 
 program.option('-p, --port <number>', 'port number')
 
 app.listen(port, () => {
-  console.log(`app is listening on port ${port}`)
+  logger.info(`app is listening on port ${port}`)
 })
