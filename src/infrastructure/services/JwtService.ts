cd92cc3dc30980fd68f0a6a0a8b991d354c299a7
Haris Javed <haris.javed.skipq@gmail.com>
2023-05-08 19:54:24 +0500

    refactored code

--- src/infrastructure/services/JwtService.ts
+++ src/infrastructure/services/JwtService.ts
@@ -1,6 +1,9 @@
-import * as jwt from 'jsonwebtoken'
 import config from '../config'
 import { injectable } from 'inversify'
+import passport = require('passport')
+import { ExtractJwt, Strategy as JwtStrategy } from 'passport-jwt'
+import { User } from '../../domain/entity/UserEntity'
+import * as jwt from 'jsonwebtoken'
 
 export interface IJwt {
   sign(payload: object): string
@@ -8,12 +11,36 @@ export interface IJwt {
 }
 
 @injectable()
-export class Jwt implements Jwt {
+export class Jwt implements IJwt {
+  constructor() {
+    const opts = {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.jwt.jwtSecretKey,
+    }
+
+    passport.use(
+      new JwtStrategy(opts, (jwtPayload, done) => {
+        if (jwtPayload) {
+          return done(null, jwtPayload)
+        }
+        return done(null, false)
+      })
+    )
+  }
+
   sign(payload: object) {
     return jwt.sign(payload, config.jwt.jwtSecretKey, { expiresIn: '10h' })
   }
 
-  verify(token: string, key: string) {
-    return jwt.verify(token, key)
+  verify(token: string, key: string): any {
+    return new Promise((resolve, reject) => {
+      passport.authenticate('jwt', { session: false }, (err: any, user: User) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(user)
+        }
+      })({ headers: { authorization: `Bearer ${token}` } })
+    })
   }
 }
