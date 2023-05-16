ef89d7bd65345263f3a9402c6e6e81ce10f0c69e
Haris Javed <haris.javed.skipq@gmail.com>
2023-05-16 14:51:32 +0500

    refactored code

--- src/domain/entity/TodoEntity.ts
+++ src/domain/entity/TodoEntity.ts
@@ -13,6 +13,22 @@ export class Todo {
 
   static todoFactory(data: { id: string; title: string; completed: boolean }): Todo {
     const { id, title, completed } = data
+
+    // Validate the id
+    if (!id || id.length === 0) {
+      throw new Error('The id must be a non-empty string.')
+    }
+
+    // Validate the title
+    if (!title || title.length === 0) {
+      throw new Error('The title must be a non-empty string.')
+    }
+
+    // Validate the completed flag
+    if (typeof completed !== 'boolean') {
+      throw new Error('The completed flag must be a boolean.')
+    }
+
     if (data.id) {
       return new Todo(id, title, completed)
     }
