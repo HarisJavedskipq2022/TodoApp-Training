ad9cff33beffda04eaf143a02b9bd60afa77212f
Haris Javed <haris.javed.skipq@gmail.com>
2023-05-16 16:22:19 +0500

    refactored code

--- src/infrastructure/config/database.ts
+++ src/infrastructure/config/database.ts
@@ -3,10 +3,10 @@ dotenv.config()
 
 export interface Database {
   databaseUrlLocal: string
-  databseUrlDocker: string
+  databaseUrlDocker: string
 }
 
 export const database: Database = {
   databaseUrlLocal: process.env.DATABASE_URL_LOCAL || '',
-  databseUrlDocker: process.env.DATABASE_URL_DOCKER || '',
+  databaseUrlDocker: process.env.DATABASE_URL_DOCKER || '',
 }
