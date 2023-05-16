6eb29bcce90a06888e63f3c77e618977cb8f0e3e
Haris Javed <haris.javed.skipq@gmail.com>
2023-05-16 15:27:53 +0500

    refactored code

--- src/domain/entity/UserEntity.ts
+++ src/domain/entity/UserEntity.ts
@@ -13,6 +13,23 @@ export class User {
 
   static userFactory(data: { id: string; email: string; password: string }): User {
     const { id, email, password } = data
+
+    if (!email || email.length === 0) {
+      throw new Error('The email must be a non-empty string.')
+    }
+
+    if (!password || password.length === 0) {
+      throw new Error('The password must be a non-empty string.')
+    }
+
+    if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
+      throw new Error('The email must be in a valid format.')
+    }
+
+    if (password.length < 8) {
+      throw new Error('The password must be at least 6 characters long.')
+    }
+
     if (data.id) {
       return new User(id, email, password)
     }
