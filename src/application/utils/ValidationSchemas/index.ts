59a054f5b5e7d718bd7558d02be6a40063c13fae
Haris Javed <haris.javed.skipq@gmail.com>
2023-05-17 18:00:53 +0500

    refactor code

--- src/application/utils/ValidationSchemas/index.ts
+++ src/application/utils/ValidationSchemas/index.ts
@@ -2,11 +2,11 @@
 import { z } from 'zod'
 
 export const CreateUserSchema = z.object({
-  email: z.string().email(),
+  email: z.string().email({ message: 'Invalid Email Address' }),
   password: z.string().min(8)
 })
 
 export const UpdatePasswordSchema = z.object({
-  id: z.string(),
+  id: z.string().uuid({ message: 'Invalid UUID' }),
   newPassword: z.string().min(8)
 })
